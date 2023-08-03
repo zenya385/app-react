f7a03ff1b3ca7808f3948b6d16a0eb0b9fae127c
Zeno Roman <zeno385@gmail.com>
2023-08-03 12:11:27 +0300

    add hooks

--- src/components/Clock/Clock.jsx
+++ src/components/Clock/Clock.jsx
@@ -1,5 +1,4 @@
 import React, {
-  Component,
   useContext,
   useEffect,
   useRef,
@@ -8,44 +7,15 @@ import React, {
 import UserContext from "../context/UserContext.jsx";
 import "./Clock.scss";
 
-// export default class OldClock extends Component {
-//   state = {
-//     time: new Date().toLocaleTimeString(),
-//   };
-
-//   intervalId = null;
-
-//   componentDidMount() {
-//     console.log("setInterval");
-
-//     this.intervalId = setInterval(
-//       () => this.setState({ time: new Date().toLocaleTimeString() }),
-//       1000
-//     );
-//   }
-
-//   componentWillUnmount() {
-//     clearInterval(this.intervalId);
-//   }
-
-//   render() {
-// return <div className="Clock__face">{this.state.time}</div>;
-//   }
-// }
-
 export default function Clock() {
   const [time, setTime] = useState(() => {
     new Date().toLocaleTimeString();
   });
   const user = useContext(UserContext);
-  // console.log("user", user);
   const intervalId = useRef(null);
-  // let intervalId = null;
 
   useEffect(() => {
-    // console.log("time", time);
     intervalId.current = setInterval(
-      // intervalId = setInterval(
       () => setTime(new Date().toLocaleTimeString()),
       1000
     );
@@ -56,9 +26,8 @@ export default function Clock() {
 
   const stop = () => {
     clearInterval(intervalId.current);
-    // clearInterval(intervalId);
   };
-  // console.log("intervalId", intervalId);
+
   return (
     <div className="Clock__face">
       {time}
--- src/components/ColorPicker/ColorPicker.js
+++ src/components/ColorPicker/ColorPicker.js
@@ -1,27 +1,62 @@
-import { PureComponent } from "react";
+// import { PureComponent } from "react";
+// import styles from './ColorPicker.module.css'
+
+// export default class ColorPicker extends PureComponent {
+//   state = {
+//     activeOptionIdx: 0,
+//   };
+
+  // setActiveIdx = index => {
+  //   this.setState({ activeOptionIdx: index });
+  // };
+
+  // makeOptionClassName = index => {
+  //   return index === this.state.activeOptionIdx
+  //     ? styles.activeOption
+  //     : styles.option;
+  // };
+
+//   render() {
+//     const { activeOptionIdx } = this.state;
+//     const { options } = this.props;
+//     const { label } = options[activeOptionIdx];
+
+//     return (
+//       <div className={styles.container}>
+//         <h2 className={styles.title}>Color Picker</h2>
+//         <p>Выбран цвет: {label}</p>
+//         <div>
+//           {options.map(({ label, color }, index) => (
+//             <button
+//               key={label}
+//               aria-label={label}
+//               className={this.makeOptionClassName(index)}
+//               style={{ backgroundColor: color }}
+//               onClick={() => this.setActiveIdx(index)}
+//             ></button>
+//           ))}
+//         </div>
+//       </div>
+//     );
+//   }
+// }
+
+import { useState } from 'react';
 import styles from './ColorPicker.module.css'
 
-export default class ColorPicker extends PureComponent {
-  state = {
-    activeOptionIdx: 0,
-  };
 
-  setActiveIdx = index => {
-    this.setState({ activeOptionIdx: index });
-  };
+export default function ColorPicker({ options }) {
+  
+  const [activeOptionIdx, setaActiveOptionIdx] = useState(0)
 
-  makeOptionClassName = index => {
-    return index === this.state.activeOptionIdx
+ const makeOptionClassName  = index => {
+    return index === activeOptionIdx
       ? styles.activeOption
       : styles.option;
   };
-
-  render() {
-    const { activeOptionIdx } = this.state;
-    const { options } = this.props;
-    const { label } = options[activeOptionIdx];
-
-    return (
+ 
+  const { label } = options[activeOptionIdx];
+  return (
       <div className={styles.container}>
         <h2 className={styles.title}>Color Picker</h2>
         <p>Выбран цвет: {label}</p>
@@ -30,13 +65,13 @@ export default class ColorPicker extends PureComponent {
             <button
               key={label}
               aria-label={label}
-              className={this.makeOptionClassName(index)}
+              className={makeOptionClassName(index)}
               style={{ backgroundColor: color }}
-              onClick={() => this.setActiveIdx(index)}
+              // eslint-disable-next-line no-undef
+              onClick={() => setaActiveOptionIdx(index)}
             ></button>
           ))}
         </div>
       </div>
     );
-  }
-}
+};
